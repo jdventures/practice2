eddccd23d03f6c05723ff893c684ca62ae622b8c
David Onquit <davidonquit@Davids-MacBook-Air-2.local>
2022-04-16 22:29:40 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const submit = document.querySelector('.submit');
+const container = document.querySelector('.container');
+const thankYou = document.querySelector('.thank-you');
+// const btn1 = document.querySelector('.btn-1');
+// const btn2 = document.querySelector('.btn-2');
+
+// console.log(btn);
+
+// btn.addEventListener('mouseover', () => {
+
+// })
+
+submit.addEventListener('click', () => {
+    container.style.display = 'none';
+    thankYou.style.display = 'block';
+    console.log('dave i love you');
+
+})
